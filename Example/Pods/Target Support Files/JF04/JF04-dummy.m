ab0f008b323b8831dc18a7348796fb279d87c432
zx <zx@zxdeiMac.local>
2019-10-10 17:09:24 +0800

    擦擦擦擦

---
+++ Example/Pods/Target Support Files/JF04/JF04-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JF04 : NSObject
+@end
+@implementation PodsDummy_JF04
+@end
---
+++ Example/Pods/Target Support Files/Pods-JF04_Tests/Pods-JF04_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JF04_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_JF04_Tests
+@end
